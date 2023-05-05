01571ffa97dba17cdbd33855139c5ffac2e9df9f
SafidyMendrika <mendrikarazafimalaza@gmail.com>
2023-05-05 16:00:57 +0300

    configurations

--- application/config/database.php
+++ application/config/database.php
@@ -77,9 +77,16 @@ $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost', //local
 	// 'hostname' => 'localhost', //deployé
-	'username' => '',
-	'password' => '',
-	'database' => '',
+
+	'username' => 'unity',// local
+	//'username' => '', //deployé
+
+	'password' => 'unity',// local
+	//'password' => 'unity45/UNITY',// deployé
+
+	'database' => 'webcup', // local
+	//'database' => 'unityteam_webcup', // deployé
+
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
