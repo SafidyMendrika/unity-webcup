7eba44f5f1cb3c20ed537d596564ac4ca142344c
MirijaMarc <mirijarazafimbelo30@gmail.com>
2023-05-07 02:00:32 +0300

    count cauchemar

--- application/models/Onirix.php
+++ application/models/Onirix.php
@@ -97,4 +97,19 @@ class Onirix extends \CI_Model
 
         return $result;
     }
+
+
+    public function countDream (){
+       // $iduser =$_SESSION['iduser'],
+        $iduser =1;
+        $result= $this->db->query('select h.iduser,p.idtypereve,tr.libele from historique h NATURAL JOIN prediction p  JOIN typereve tr ON tr.id = p.idtypereve WHERE p.idtypereve=1 and h.iduser= '.$iduser);
+        return $result->num_rows();
+    }
+
+    public function countCauchemar (){
+        // $iduser =$_SESSION['iduser'],
+        $iduser =1;
+        $result= $this->db->query('select h.iduser,p.idtypereve,tr.libele from historique h NATURAL JOIN prediction p  JOIN typereve tr ON tr.id = p.idtypereve WHERE p.idtypereve=2 and h.iduser= '.$iduser);
+        return $result->num_rows();
+    }
 }
