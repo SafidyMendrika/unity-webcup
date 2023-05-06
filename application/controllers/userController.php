8b2d610c2222e3a0aeb60327decf009fd7e0188f
SafidyMendrika <mendrikarazafimalaza@gmail.com>
2023-05-06 20:36:56 +0300

    modification prompt

--- application/controllers/userController.php
+++ application/controllers/userController.php
@@ -14,7 +14,22 @@ class userController extends CI_Controller
     public function id($id){
         $usr = new User();
          var_dump($usr);
+    }
+    public function mail(){
 
+        $from_email = "safidymendrika823@gmail.com";
+        $to_email = "mendrikarazafimalaza@gmail.com";
+        $this->load->library('email');
 
+        $this->email->from($from_email, 'BADOUDE');
+        $this->email->to($to_email);
+        $this->email->subject('Email Test');
+        $this->email->message('Testing the email class leaaka badoude');
+
+        if ($this->email->send()){
+            echo "lasaaaaa";
+        }else{
+            echo "tsy lasaaaaaa";
+        }
     }
 }
