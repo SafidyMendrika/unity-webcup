f01258b9255d6dc0b0d7dbaf1d507f82570e9a45
MirijaMarc <mirijarazafimbelo30@gmail.com>
2023-05-06 23:35:53 +0300

    base

--- application/models/script/base.sql
+++ application/models/script/base.sql
@@ -10,3 +10,39 @@ create table user(
     password varchar(255),
     idgoogleclient varchar(255)
 );
+
+create table typereve(
+  id int auto_increment PRIMARY KEY,
+  libele varchar(50) not null
+);
+
+create table motcle(
+  id int auto_increment PRIMARY KEY,
+  mot varchar(50) not null
+);
+
+
+create table categorieprediction(
+  id int auto_increment PRIMARY KEY,
+  nomcategorie varchar(50) not null
+);
+
+create table prediction(
+  id int auto_increment PRIMARY KEY,
+  prediction text not null,
+  idtypereve int references typereve(id),
+  idcategorieprediction int references categorieprediction(id)
+);
+
+create table prediction_motcle(
+  idmotcle int references motcle(id),
+  idprediction int references prediction(id)
+
+);
+
+create table historique(
+  id int auto_increment PRIMARY KEY,
+  iduser int references user(id),
+  idprediction int references prediction(id)
+);
+ 
