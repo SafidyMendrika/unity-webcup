48dc44ff58302fba543edb313923c6dac1a276a4
SafidyMendrika <mendrikarazafimalaza@gmail.com>
2023-05-07 03:27:45 +0300

    specialist

---
+++ application/controllers/Specialist.php
@@ -0,0 +1,12 @@
+<?php
+
+class Specialist extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function detals($specialite){
+        
+    }
+}
