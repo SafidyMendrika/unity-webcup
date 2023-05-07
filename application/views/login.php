92ac5a639fa5d91b31a868707b75ce665abdf408
RJToky <tokyrakotoarivony5@gmail.com>
2023-05-07 03:28:27 +0300

    vita front login

---
+++ application/views/login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/login.css"); ?>">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+        <div class="left">
+            <h1>Se connecter</h1>
+            <form action="#">
+                <div class="field">
+                    <label for="">Nom d'utilisateur</label>
+                    <input type="text" placeholder="Nom d'utilisateur">
+                </div>
+                <div class="field">
+                    <label for="">Mot de passe</label>
+                    <input type="password" placeholder="Mot de passe">
+                </div>
+                <div class="buttons">
+                    <div>
+                        <input type="submit" value="Se connecter">
+                    </div>
+                    <div>OU</div>
+                    <a href="#">
+                        <span>
+                            <img src="<?php echo base_url("assets/icon/google-color-svgrepo-com.svg"); ?>" alt="">
+                        </span>
+                        <span>Connexion avec Google</span>
+                    </a>
+                </div>
+            </form>
+        </div>
+        <div class="right">
+            <img src="" alt="">
+        </div>
+    </div>
+</body>
+</html>
