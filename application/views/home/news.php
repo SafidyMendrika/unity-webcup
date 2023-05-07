a4054f2a34660c5919e4d8ae6e288bcee451b33f
MirijaMarc <mirijarazafimbelo30@gmail.com>
2023-05-07 08:04:22 +0300

    actu

--- application/views/home/news.php
+++ application/views/home/news.php
@@ -5,21 +5,21 @@
             <div class="bg"></div>
             <div class="text">
                 <h1>Actualité 1</h1>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa placeat repudiandae a voluptate!</p>
+                <p>Google utilise l'IA pour détecter les maladies cardiovasculaires en analysant les échographies cardiaques.</p>
             </div>
         </div>
         <div class="card-news">
             <div class="bg"></div>
             <div class="text">
                 <h1>Actualité 2</h1>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa placeat repudiandae a voluptate!</p>
+                <p>L'Université Carnegie Mellon a développé une technique d'IA pour aider les robots à comprendre leur environnement.</p>
             </div>
         </div>
         <div class="card-news">
             <div class="bg"></div>
             <div class="text">
                 <h1>Actualité 3</h1>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa placeat repudiandae a voluptate!</p>
+                <p>Les modèles de langage d'IA peuvent être biaisés en raison des données d'entraînement.</p>
             </div>
         </div>
     </div>
