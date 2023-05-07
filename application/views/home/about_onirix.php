3ece29983c7c06390c13a7da0c98dae2b09789e5
RJToky <tokyrakotoarivony5@gmail.com>
2023-05-07 07:50:50 +0300

    vita onirix

--- application/views/home/about_onirix.php
+++ application/views/home/about_onirix.php
@@ -1,9 +1,11 @@
 <section class="section about-onirix">
+    <div class="logo">
+        <img src="<?php echo base_url("assets/img/logo-onirix.png"); ?>" alt="">
+    </div>
     <h1>ONIRIX, c'est quoi ?</h1>
     <p>
         ONIRIX est une intelligence artificielle developee par l'institut international des Reves.<br />
         C'est une IA avancee capable d'analyser et de decoder les reves des utilisateurs pour predire leur avenir.<br />
         Elle permet aux utilisateurs de soumettre facilement la description de leurs reves pour obtenir des predictions premonitoires personnalisee et des informations utiles sur les evenements a venir!
     </p>
-    <img src="" alt="">
 </section>
