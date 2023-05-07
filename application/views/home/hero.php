0bd0f1a3dfd71c92ff42c29e63e520abe976848b
RJToky <tokyrakotoarivony5@gmail.com>
2023-05-07 05:57:14 +0300

    checkpoint

--- application/views/home/hero.php
+++ application/views/home/hero.php
@@ -7,15 +7,15 @@
     <div class="bottom">
         <div>
             <h1>Predictions</h1>
-            <p>Onirix permet de predire des situations telles qu'une rencontre amoureuse, des problemes avec des collegues de travail, des difficulte scolaires ou meme des maladies graves.</p>
+            <p>Onirix prédit rencontres amoureuses, problèmes travail, difficultés scolaires et maladies graves.</p>
         </div>
         <div>
             <h1>Sens</h1>
             <p>Peut vous donner un sens au reve que vous avez experience et decris.</p>
         </div>
         <div>
-           <h1>Title 2</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do.</p>
+           <h1>Potentiel</h1>
+            <p>Une IA interprétant les rêves pour prédire l'avenir ? L'impact serait immense.</p>
         </div>
     </div>
 </section>
